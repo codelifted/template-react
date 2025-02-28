96ce5a8e66ca776185edd8a6b0a6788b7d752dda
Ubuntu <andkho@azure-vm-codelifted-vm-andrey-andkho-vm.ct34xnmniuvujma0lcjek4okdc.ax.internal.cloudapp.net>
2025-02-28 14:47:09 +0000

    add keycloak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+  url: process.env.REACT_APP_KEYCLOAK_URL,
+  realm: 'saas-hello-world-auth',
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required', pkceMethod: 'S256' }).then(auth => {
+      if (auth) {
+        console.log('Authenticated', keycloak.token);
+        fetch('http://hello-backend.saas-hello-world.svc.cluster.local:3000/api', {
+          headers: { Authorization: `Bearer ${keycloak.token}` },
+        });
+      }
+    });
+  }, []);
+
+  return <div>Hello from React!</div>;
 }
 
 export default App;
